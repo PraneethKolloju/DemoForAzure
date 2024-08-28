6f941bc59083cca08f4f4f3f9e3698281940308d
PraneethKolloju <67681816+PraneethKolloju@users.noreply.github.com>
2024-08-28 23:56:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CI CD Pipe Lines Implementation on Azure DevOps
         </p>
         <a
           className="App-link"
